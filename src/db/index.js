f463882c47ea52396f1b68a7659e6c3320d856a0
Yatri Patel <yatri.8424@gmail.com>
2025-03-10 09:31:48 +0530

    utils code discussion

--- src/db/index.js
+++ src/db/index.js
@@ -13,3 +13,11 @@ const connectDB = async () => {
 }
 
 export default connectDB
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection fail !!!", err);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+// asyncHandler.js file method bana ke usko export ke liye banani hoti hai
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}  // (or) try-catch se bhi kar sakte hai
+
+
+export {asyncHandler}
+
+// higher order function: jo function ko as a parameter bhi exept kar sakte hai ya to usko return kar sakte hai
+// yahan pe humne parameter me hi ek fn(function) as a parameter liya hai
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => { () => {} }
+// const asyncHandler = (func) => { async () => {} }
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+    try{
+        await fn(req, res, next)
+    }
+    catch(error){
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+*/
